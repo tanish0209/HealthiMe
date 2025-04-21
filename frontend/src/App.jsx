856df907aefb0fffc04707749115e9a095a07d2e
tanish0209 <112083355+tanish0209@users.noreply.github.com>
2025-04-21 13:40:02 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -19,14 +19,14 @@ const App = () => {
       <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/doctors" element={<Doctors />} />
-        <Route path="/doctors/:speciality" element={<Doctors />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/my-profile" element={<MyProfile />} />
-        <Route path="/my-appointments" element={<MyAppointments />} />
-        <Route path="/appointment/:docId" element={<Appointment />} />
+        <Route path="/doctors/" element={<Doctors />} />
+        <Route path="/doctors/:speciality/" element={<Doctors />} />
+        <Route path="/login/" element={<Login />} />
+        <Route path="/about/" element={<About />} />
+        <Route path="/contact/" element={<Contact />} />
+        <Route path="/my-profile/" element={<MyProfile />} />
+        <Route path="/my-appointments/" element={<MyAppointments />} />
+        <Route path="/appointment/:docId/" element={<Appointment />} />
       </Routes>
       <Footer />
     </div>
