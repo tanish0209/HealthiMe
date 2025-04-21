4cb579bd180e8376c6b806c81083138c35b07e17
tanish0209 <112083355+tanish0209@users.noreply.github.com>
2025-04-21 14:08:00 +0530

    Update DoctorContext.jsx

--- admin/src/context/DoctorContext.jsx
+++ admin/src/context/DoctorContext.jsx
@@ -4,7 +4,7 @@ import { toast } from "react-toastify";
 export const DoctorContext = createContext();
 
 const DoctorContextProvider = (props) => {
-  const backendUrl = 'https://healthime.vercel.app/';
+  const backendUrl = 'https://healthime-backend.onrender.com';
   const [dToken, setDToken] = useState(
     localStorage.getItem("dToken") ? localStorage.getItem("dToken") : ""
   );
