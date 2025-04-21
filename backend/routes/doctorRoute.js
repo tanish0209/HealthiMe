079e35cad5f026ccfe8962182f720b1df9631655
tanish0209 <112083355+tanish0209@users.noreply.github.com>
2025-04-21 13:44:10 +0530

    Update doctorRoute.js

--- backend/routes/doctorRoute.js
+++ backend/routes/doctorRoute.js
@@ -2,7 +2,7 @@ import express from 'express';
 import { appointmentCancel, appointmentComplete, appointmentsDoctor, doctorDasboard, doctorList, doctorProfile, loginDoctor, updateDoctorProfile } from '../controllers/doctorController.js';
 import authDoctor from '../middlewares/authDoctor.js';
 const doctorRouter = express.Router()
-doctorRouter.get('/list', doctorList)
+doctorRouter.get('/list/', doctorList)
 doctorRouter.post('/login', loginDoctor)
 doctorRouter.get('/appointments', authDoctor, appointmentsDoctor)
 doctorRouter.post('/complete-appointment', authDoctor, appointmentComplete)
